ba465cbf299c8c45e34e2decf78300ba0196293d
Dave Hernandez <brodave318@gmail.com>
2018-02-16 19:23:32 -0800

    Creation of Todos w/ functions"

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,23 @@
 // Check Off Specific Todos by Clicking
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
   $(this).toggleClass('completed');
 });
 
 // Click on X to delete Todo
-$("li span").click(function(event) {
+$("ul").on('click', 'span', function(event) {
   $(this).parent().fadeOut(500, function() {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+// Create new Todo
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    // Grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val('');
+    // Create a new li and add to ul
+    $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+  }
 });
